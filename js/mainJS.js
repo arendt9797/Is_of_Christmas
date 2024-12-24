e9b22f88e637d5b9b18115c7fbbef05270bd9b5c
verdantgreeny <verdantgreeny@gmail.com>
2024-12-24 20:33:26 +0900

    메인변경

--- js/mainJS.js
+++ js/mainJS.js
@@ -1,7 +1,12 @@
+
+
+
 function open_card() {
     document.getElementById('front').className = 'openCard';
-  }
-  
+    rudolphAudio.play();
+
+  };
+
   function close_card() {
     document.getElementById('front').className = '';
   }
