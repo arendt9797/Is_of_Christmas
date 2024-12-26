7e9a9c48737dd98cade509ced5cea88b24d6fa9d
ickhwan YU <ickhwanyu@icks-MacBook-Pro.local>
2024-12-26 20:41:21 +0900

    배경음악 추가

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-function start_Bgm() {
-    document.getElementById('jingleBell');
-    christmasBgm.play();
-    christmasBgm.volume = 0.1;
-};
-// var x = document.getElementById("christmasBgm");
+// function start_Bgm() {
+//     document.getElementById('jingleBell');
+//     christmasBgm.play();
+//     christmasBgm.volume = 0.1;
+// };
 
-// function playAudio() {
-//     x.play();
-// }
+const playPauseButton = document.getElementById("jingleBell");
+const audio = document.getElementById("christmasBgm");
+// const icon = document.getElementById("icon");
 
-// function pauseAudio() {
-//     x.pause();
-// }
+playPauseButton.addEventListener("click", () => {
+    console.log('hello')
+    if (audio.paused) {
+        audio.play();
+        audio.volume = 0.1;
+        // icon.textContent = "⏸️"; // Change to Pause icon
+        // playPauseButton.innerHTML = `<span id="icon">⏸️</span> Pause Music`;
+    } else {
+        audio.pause();
+        audio.volume = 0.1;
+        // icon.textContent = "▶️"; // Change to Play icon
+        // playPauseButton.innerHTML = `<span id="icon">▶️</span> Play Music`;
+    }
+});
